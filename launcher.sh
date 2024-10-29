fab485041fb041877e06be056c67cc03e1926d87
roshantaneja <rytaneja@gmail.com>
2024-10-29 00:39:55 -0700

    fix launcher

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/roshan/Downloads/doomsday-clock
+cd home/roshan/doomsday-clock
 sudo python3 doomsday.py
 cd /
