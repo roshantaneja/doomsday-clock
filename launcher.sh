428e7cbf0bb17eaf5e9a29ca644f4d2d25b93c7f
roshantaneja <rytaneja@gmail.com>
2024-12-09 16:44:11 -0800

    parametric launcher options

--- launcher.sh
+++ launcher.sh
@@ -2,8 +2,19 @@
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
-cd /
-cd home/roshan/doomsday-clock
+#!/bin/bash
+
+# Get the directory of the script itself
+script_dir=$(dirname "$0")
+
+# Navigate to that directory
+cd "$script_dir"
+
+# Now you are in the same directory as the script file
+echo "Current directory:"
+pwd
+
+
 git stash
 git pull
 python3 doomsday.py
